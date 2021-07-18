e10048f4df456c6e928ff0634a4366c2944775da
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-18 13:04:51 +0700

    Navigation & Route Parameter

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import Weather from "./components/Weather";
-import ZipCodeScreen from "./components/ZipCodeScreen";
+import ZipCodeScreen from "./screen/ZipCodeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import WeatherScreen from "./screen/WeatherScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
